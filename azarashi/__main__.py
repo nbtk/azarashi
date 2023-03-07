cd0916efc3837a30290697d065591578a6ddbc4e
Nobutaka Horie <nobutaka@horie.dev>
2023-03-07 11:14:12 +0000

    verboseオプションを追加した。レコードのアトリビュートを表示できる。

--- azarashi/__main__.py
+++ azarashi/__main__.py
@@ -1,6 +1,7 @@
 import sys
 import datetime
 import argparse
+from pprint import pformat
 from azarashi import QzssDcrDecoderException
 from azarashi import decode_stream
 
@@ -8,8 +9,9 @@ from azarashi import decode_stream
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('type', help='set a message type: "nmea", "ublox" or "hex"', type=str)
-    parser.add_argument('--source', help='output source messages', action='store_true')
-    parser.add_argument('--unique', help='supress duplicate messages', action='store_true')
+    parser.add_argument('-s', '--source', help='output the source messages', action='store_true')
+    parser.add_argument('-u', '--unique', help='supress duplicate messages', action='store_true')
+    parser.add_argument('-v', '--verbose', help="verbose mode", action='store_true')
     args = parser.parse_args()
     if args.type not in ('hex', 'nmea', 'spresense', 'ublox'): # 'spresense' is there for compatibility, it is exactly the same as 'nmea'.
         raise NotImplementedError('The message type must be specified as "nmea", "ublox" or "hex"')
@@ -18,13 +20,18 @@ def main():
         now = datetime.datetime.now().isoformat()
         try:
             report = decode_stream(sys.stdin, args.type, unique=args.unique)
-            print(f'{now} --------------------------------\n{report}\n')
+            if args.verbose is True:
+                print(f'{now} --------------------------------\n{pformat(report.get_params())}\n')
+            else:
+                print(f'{now} --------------------------------\n{report}\n')
+
             if args.source is True:
                 if type(report.sentence) is bytes:
                     src = "b'" + ''.join(r'\x%02X' % c for c in report.sentence) + "'"
                 else:
                     src = report.sentence
                 print(f'# src: {src}\n# hex: {report.message.hex().upper()[:-1]}\n')
+
             sys.stdout.flush()
         except QzssDcrDecoderException as e:
             print(f'{now} --------------------------------\n# [{type(e).__name__}] {e}\n', file=sys.stderr)
