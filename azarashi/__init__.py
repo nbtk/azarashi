bf706149908d02f4fd95bf9b01a3daff1de32cc8
Nobutaka Horie <nobutaka@horie.dev>
2023-03-17 10:00:39 +0000

    qzss_dc_reportのエイリアスを追加

--- azarashi/__init__.py
+++ azarashi/__init__.py
@@ -2,3 +2,4 @@ from .qzss_dcr_lib.exception import QzssDcrDecoderException
 from .qzss_dcr_lib.exception import QzssDcrDecoderNotImplementedError
 from .qzss_dcr_lib.interface import decode
 from .qzss_dcr_lib.interface import decode_stream
+from .qzss_dcr_lib.report import qzss_dc_report
