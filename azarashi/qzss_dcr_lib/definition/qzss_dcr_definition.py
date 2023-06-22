cc165f640df06297ebc3e3271ddfd6714eaf7380
Nobutaka Horie <nobutaka@horie.dev>
2023-06-22 12:13:02 +0900

    タイポを修正した

--- azarashi/qzss_dcr_lib/definition/qzss_dcr_definition.py
+++ azarashi/qzss_dcr_lib/definition/qzss_dcr_definition.py
@@ -2,20 +2,20 @@ class QzssDcrDefinition(dict):
     def __init__(self,
                  default,
                  prefix=None,
-                 prefix_extructor=None,
+                 prefix_extractor=None,
                  undefined=None,
                  *args,
                  **kwargs):
         super().__init__(default, *args, **kwargs)
         self.prefix = prefix
-        self.prefix_extructor = prefix_extructor
+        self.prefix_extractor = prefix_extractor
         self.undefined = undefined
 
     def __missing__(self, key):
         value = None
 
-        if self.prefix and self.prefix_extructor:
-            value = self.prefix.get(self.prefix_extructor(key))
+        if self.prefix and self.prefix_extractor:
+            value = self.prefix.get(self.prefix_extractor(key))
             if value is not None:
                 value = value % key
 
