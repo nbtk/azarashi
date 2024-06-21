c780a2a6ac8d3eaf36c0658543e989ed9b18bb03
Nobutaka Horie <nobutaka@horie.dev>
2024-06-21 16:52:42 +0900

    バージョン番号を更新

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='azarashi',
-    version='0.10.0',
+    version='0.10.1',
     description='QZSS DCR Decoder',
     long_description=long_description,
     long_description_content_type='text/markdown',
