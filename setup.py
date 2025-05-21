4c43140f91cc438101ff450063eac0861c9ff171
nbtk <horie@bitmeister.jp>
2025-05-21 21:18:04 +0900

    Update version to 0.12.0 and add PyPI release workflow
    Bumped the package version from 0.11.0 to 0.12.0. Introduced a GitHub Actions workflow to automate building and publishing the package to PyPI upon new releases. This ensures streamlined and consistent deployment of new versions.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='azarashi',
-    version='0.11.0',
+    version='0.12.0',
     description='QZSS DCR Decoder',
     long_description=long_description,
     long_description_content_type='text/markdown',
