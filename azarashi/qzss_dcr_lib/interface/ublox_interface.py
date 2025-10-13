8ecddfb6a6de0e8b97fa7f4d073e8a794e6bb368
nbtk <nobutaka@horie.dev>
2025-10-14 00:03:47 +0900

    リファクタリング

--- azarashi/qzss_dcr_lib/interface/ublox_interface.py
+++ azarashi/qzss_dcr_lib/interface/ublox_interface.py
@@ -1,13 +1,21 @@
+import struct
+
 from ..definition import qzss_dcr_message_type
 from ..definition import ublox_qzss_dcr_message_header
 
 buffers = {}
 
 
-def __pop(size, reader, reader_args=(), reader_kwargs={}):
+def __pop(size, reader, reader_args=None, reader_kwargs=None):
     buf = buffers.get(reader)
     if buf is None:
         buf = b''
+
+    if reader_args is None:
+        reader_args = ()
+
+    if reader_kwargs is None:
+        reader_kwargs = {}
 
     while size > len(buf):
         data = reader(*reader_args, **reader_kwargs)
@@ -21,7 +29,14 @@ def __pop(size, reader, reader_args=(), reader_kwargs={}):
     return ret
 
 
-def ublox_qzss_dcr_message_extractor(reader, reader_args=(), reader_kwargs={}):
+def ublox_qzss_dcr_message_extractor(reader, reader_args=None, reader_kwargs=None):
+
+    if reader_args is None:
+        reader_args = ()
+
+    if reader_kwargs is None:
+        reader_kwargs = {}
+
     match_count = 0
     while True:
         if ublox_qzss_dcr_message_header[match_count] == __pop(1,
@@ -32,25 +47,21 @@ def ublox_qzss_dcr_message_extractor(reader, reader_args=(), reader_kwargs={}):
         else:
             match_count = 0
 
-        if match_count == len(ublox_qzss_dcr_message_header):  # SFRBX message
+        if match_count == len(ublox_qzss_dcr_message_header): # SFRBX message
             match_count = 0
-            
-            message_length_lsb = __pop(1,reader,reader_args,reader_kwargs)[0]
-            
-            message_length_msb = __pop(1,reader,reader_args,reader_kwargs)[0]
-            
-            message_length = message_length_lsb + \
-                             (message_length_msb << 8)  # (little-endian 16-bit integer)
-            
+            message_length_bytes = __pop(2,
+                                         reader,
+                                         reader_args,
+                                         reader_kwargs)
+            message_length = struct.unpack('<H', message_length_bytes)[0]
             message = ublox_qzss_dcr_message_header + \
-                      message_length_lsb.to_bytes(1,'little') + \
-                      message_length_msb.to_bytes(1,'little') + \
-                      __pop(message_length + 2 , # Payload + CK_A + CK_B
+                      message_length_bytes + \
+                      __pop(message_length + 2 , # payload + CK_A + CK_B
                             reader,
                             reader_args,
                             reader_kwargs)
             
-            if message[6] != 5:  # gnssID != QZSS
+            if message[6] != 5:  # the message is not sent by QZSS
                 continue
 
             if message[8] != 1:  # not a L1S signal
