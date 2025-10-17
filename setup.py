ea2e2d78daa4be3edcf4bde6890cdb1bf1e06ad8
nbtk <horie@bitmeister.jp>
2025-10-18 00:12:13 +0900

    バージョン番号更新

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='azarashi',
-    version='0.12.0',
+    version='0.15.0',
     description='QZSS DCR Decoder',
     long_description=long_description,
     long_description_content_type='text/markdown',
