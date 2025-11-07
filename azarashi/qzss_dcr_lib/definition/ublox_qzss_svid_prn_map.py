7e5246dfecb2ce51062979e6f00ac3f63eb9165d
nezumi_tech <nezumi_tech@yahoo.co.jp>
2025-11-07 16:36:08 +0900

    Rename and update ublox QZSS SVID-PRN map
    ublox_qzss_svn_prn_map.py を ublox_qzss_svid_prn_map.py にリネーム。
    u-bloxのsvIdとL1SのPRNの対応付けを修正。

--- azarashi/qzss_dcr_lib/definition/ublox_qzss_svid_prn_map.py
+++ azarashi/qzss_dcr_lib/definition/ublox_qzss_svid_prn_map.py
@@ -1,12 +1,11 @@
 from .qzss_dcr_definition import QzssDcrDefinition
 
-ublox_qzss_svn_prn_map = QzssDcrDefinition(
+ublox_qzss_svid_prn_map = QzssDcrDefinition(
     {
-        1: 183,
         2: 184,
-        3: 189,
-        4: 185,
-        5: 186,
+        3: 185,
+        4: 186,
+        7: 189,
     },
     undefined = 183 # has already gone silent, but it is still used as the default value
 )
