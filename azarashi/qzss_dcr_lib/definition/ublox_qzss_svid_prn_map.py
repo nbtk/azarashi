7505ea75c7541c624d5a916d9de47dd2e0ca0068
nezumi_tech <nezumi_tech@yahoo.co.jp>
2025-11-08 21:09:59 +0900

    ublox_qzss_svid_prn_mapをQzssDcrDefinitionからdict型に戻した。

--- azarashi/qzss_dcr_lib/definition/ublox_qzss_svid_prn_map.py
+++ azarashi/qzss_dcr_lib/definition/ublox_qzss_svid_prn_map.py
@@ -1,11 +1,6 @@
-from .qzss_dcr_definition import QzssDcrDefinition
-
-ublox_qzss_svid_prn_map = QzssDcrDefinition(
-    {
+ublox_qzss_svid_prn_map : dict[int, int] = {
         2: 184,
         3: 185,
         4: 186,
         7: 189,
-    },
-    undefined = 183 # has already gone silent, but it is still used as the default value
-)
+}
