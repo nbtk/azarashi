e72b7101105db1d0db9e0139f718acf74f7d6e37
NBTK <7089467+nbtk@users.noreply.github.com>
2026-05-10 17:37:28 +0900

    Bump version from 0.15.1 to 0.16.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setuptools.setup(
     name='azarashi',
-    version='0.15.1',
+    version='0.16.0',
     description='QZSS DCR Decoder',
     long_description=long_description,
     long_description_content_type='text/markdown',
