df317fb2fc1c39fd73fd52befefa4079821a26be
あるふぁ米 <206510188+A-vrice@users.noreply.github.com>
2026-05-12 08:19:18 +0900

    fiexed typo

--- azarashi/qzss_dcr_lib/definition/qzss_dcr_jma_tsunamigenic_potential.py
+++ azarashi/qzss_dcr_lib/definition/qzss_dcr_jma_tsunamigenic_potential.py
@@ -6,8 +6,8 @@ qzss_dcr_jma_tsunamigenic_potential_en = QzssDcrDefinition(
         1: "There is a Possibility of a Destructive Ocean-Wide Tsunami",
         2: "There is a Possibility of a Destructive Regional Tsunami",
         3: "There is a Possibility of a Destructive Local Tsunami Near the Epicenter",
-        4: "There is a Very Small Possibility of a destructive Local Tsunami",
-        7: "There is Possibility of a Tsunami",  # "その他の津波発生の可能性有無",
+        4: "There is a Very Small Possibility of a Destructive Local Tsunami",
+        7: "There is a Possibility of a Tsunami",  # "その他の津波発生の可能性有無",
         # "N*": "Tsunamigenic Potential (Code: N)",
     },
     undefined="Undefined Tsunamigenic Potential (Code: %d)"
