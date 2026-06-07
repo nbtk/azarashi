4ea3b0e370c7d02f9d9d4f72faf86e0a62af367d
A-vrice <206510188+A-vrice@users.noreply.github.com>
2026-06-07 23:10:22 +0900

    feat: implement UDP network receiver and transmitter modules for QZSS DCR data

--- test/test_helpers.h
+++ test/test_helpers.h
@@ -96,7 +96,6 @@ inline std::string makeNmeaQzqsm(uint8_t svid, const uint8_t* nav_bits) {
     sprintf(hex, "%01X", (nav_bits[31] >> 4) & 0xF);
     s += hex;
 
-
     uint8_t xsum = 0;
     for (size_t i = 1; i < s.size(); ++i) {
         xsum ^= (uint8_t)s[i];
